5d2b90940a8e482e9c0722ec1753e599d63a9be9
m9795 <106650955+m9795@users.noreply.github.com>
2022-06-30 18:47:55 +0900

    [Add]カラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_26_033556) do
+ActiveRecord::Schema.define(version: 2022_06_30_093610) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2021_10_26_033556) do
     t.float "rate"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "tag"
   end
 
   create_table "favorites", force: :cascade do |t|
